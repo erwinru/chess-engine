2935fa26c53708664a3d8f6475df85e6bdd2c357
Erwin Rudi <erwin.rudi@gmail.com>
2020-04-13 16:04:55 +0200

    go through moves istead of positions + alpha beta pruning

--- myproject/ai.py
+++ myproject/ai.py
@@ -3,70 +3,85 @@ import numpy as np
 
 
 class AI:
-    depth = 2
+    depth = 3
 
     def __init__(self, board, is_player_white):
         self.b = board
         self.is_ai_white = not is_player_white
-
         self.board_caches = {}
-        pass
 
-    def minimax(self, position, depth, is_maxing_white):
-        if depth == 0:
+    def minimax(self, depth, is_maxing_white, alpha, beta):
+        # if board in cache
+        if self.hash_board(depth, is_maxing_white) in self.board_caches:
+            return self.board_caches[self.hash_board(depth, is_maxing_white)]
+
+        # if depth is 0 or game is over
+
+        if depth == 0:  # or len(self.b.get_all_legal_moves()) == 0
             self.board_caches[
-                self.hash_board(position, depth, is_maxing_white)
-            ] = self.eval_board(position)
-            return self.board_caches[self.hash_board(position, depth, is_maxing_white)]
+                self.hash_board(depth, is_maxing_white)
+            ] = self.eval_board()
+            return self.board_caches[self.hash_board(depth, is_maxing_white)]
 
         best_score = float("-inf") if is_maxing_white else float("inf")
+        for move in self.b.get_all_legal_moves():
 
-        for child_pos in self.b.get_child_positions(position, is_maxing_white):
-            local_score = self.minimax(child_pos, depth - 1, False)
+            moved_piece = self.b.push(move)
+
+            local_score = self.minimax(depth - 1, not is_maxing_white, alpha, beta)
+
             self.board_caches[
-                self.hash_board(position, depth, is_maxing_white)
+                self.hash_board(depth - 1, not is_maxing_white)
             ] = local_score
 
-        if is_maxing_white:
-            best_score = max(best_score, local_score)
+            if is_maxing_white:
+                best_score = max(best_score, local_score)
+                alpha = max(alpha, best_score)
 
-        else:
-            best_score = min(best_score, local_score)
+            else:
+                best_score = min(best_score, local_score)
+                beta = min(beta, best_score)
 
-        self.board_caches[
-            self.hash_board(position, depth, is_maxing_white)
-        ] = best_score
+            self.b.pop(moved_piece, move[0])
 
-        return self.board_caches[self.hash_board(position, depth, is_maxing_white)]
+            if beta <= alpha:
+                break
 
-    def hash_board(self, position, depth, is_maxing_white):
-        return str(position) + " " + str(depth) + " " + str(is_maxing_white)
+        self.board_caches[self.hash_board(depth, is_maxing_white)] = best_score
+        return self.board_caches[self.hash_board(depth, is_maxing_white)]
 
-    def eval_board(self, position):
-        return sum([piece.value for piece in self.b.get_pieces(position)])
+    def hash_board(self, depth, is_maxing_white):
+        return str(self.b.board[-1]) + " " + str(depth) + " " + str(is_maxing_white)
 
-    def move(self, position):
+    def eval_board(self):
+        return sum([piece.value for piece in self.b.get_pieces()])
+
+    def move(self):
         global_score = float("-inf") if self.is_ai_white else float("inf")
-        chosen_move = None
+        chosen_pos = None
 
-        for child_pos in self.b.get_child_positions(position, self.is_ai_white):
+        for move in self.b.get_all_legal_moves():
 
-            local_score = self.minimax(child_pos, self.depth - 1, not self.is_ai_white)
+            moved_piece = self.b.push(move)
+
+            local_score = self.minimax(self.depth - 1, not self.is_ai_white, -1e8, 1e8)
             self.board_caches[
-                self.hash_board(child_pos, self.depth - 1, not self.is_ai_white)
+                self.hash_board(self.depth - 1, not self.is_ai_white)
             ] = local_score
 
             if self.is_ai_white and local_score > global_score:
                 global_score = local_score
-                chosen_pos = child_pos
+                chosen_pos = move
             elif not self.is_ai_white and local_score < global_score:
                 global_score = local_score
-                chosen_pos = child_pos
+                chosen_move = move
 
-            print(local_score, child_pos)
+            print(local_score, self.b.board[-1])
+            self.b.pop(moved_piece, move[0])
+
+        moved_piece = self.b.push(chosen_move)
 
         print(str(global_score) + " " + str(chosen_move) + "\n")
 
-        self.b.board = chosen_pos
-        moved_piece = self.b.update_piece_pos(chosen_pos)
-        return chosen_pos, moved_piece
+        moved_piece = self.b.update_piece_pos2(moved_piece, chosen_move[1])
+        return moved_piece
